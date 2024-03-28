95a9afba641f379dab489f79447c4e756ab2c20a
Aaryan-9 <2022kucp1031@iiitkota.ac.in>
2024-03-28 22:03:56 +0530

    nono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { SparklesPreview } from "@/components/aboutSection";
+import { TextGenerateEffectDemo } from "@/components/aboutText";
 import React from "react";
 
 function hi() {
   return (
     <>
       <SparklesPreview />
+
+      <TextGenerateEffectDemo />
     </>
   );
 }
---
+++ src/components/aboutText.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+const words = `Just when someone said - "Kota ke kaunse junglo se aate ho?"`;
+
+export function TextGenerateEffectDemo() {
+  return <TextGenerateEffect words={words} />;
+}
