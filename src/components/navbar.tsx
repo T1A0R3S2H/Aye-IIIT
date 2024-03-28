821ecc478214243095df0f3297b1c7e3bdfb9c27
Aaryan-9 <2022kucp1031@iiitkota.ac.in>
2024-03-28 21:19:20 +0530

    Defining sections

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,40 +1,51 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-
 function navbar({ className }: { className?: string }) {
-    const [active, setActive] = useState<string | null>(null);
-    return (
-        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-            <Menu setActive={setActive}>
-                
-                <Link href={"/contact"} >
-                    <MenuItem setActive={setActive} active={active} item="Contact Us">
-                    </MenuItem>
-                </Link>
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/About"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="About"
+          ></MenuItem>
+        </Link>
 
-                <Link href={"/contact"} >
-                    <MenuItem setActive={setActive} active={active} item="Contact Us">
-                    </MenuItem>
-                </Link>
+        <Link href={"/Members"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Members"
+          ></MenuItem>
+        </Link>
 
-                <Link href={"/contact"} >
-                    <MenuItem setActive={setActive} active={active} item="Contact Us">
-                    </MenuItem>
-                </Link>
+        <Link href={"/Gallery"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Gallery"
+          ></MenuItem>
+        </Link>
 
-                <Link href={"/contact"} >
-                    <MenuItem setActive={setActive} active={active} item="Contact Us">
-                    </MenuItem>
-                </Link>
-                
-            </Menu>
-        </div>
-    )
+        <Link href={"/FAQs"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="FAQs"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
 }
 
-export default navbar
+export default navbar;
